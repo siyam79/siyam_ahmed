97150318fe67b11c46b563a1166debfcfe21fa85
siyam79 <siyamahmed3827@gmail.com>
2024-03-05 16:37:13 +0600

    skills section modifiy

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import About from "./Components/About"
 import Banner from "./Components/Banner"
+import Bar from "./Components/Bar"
+import Contact from "./Components/Contact"
 import Header from "./Components/Header"
 import Nav from "./Components/Nav"
 import Services from "./Components/Services"
@@ -11,7 +13,6 @@ function App() {
 
   return (
     <>
-
       <div className='bg-site bg-no-repeat bg-cover overflow-hidden'>
         <Header></Header>
         <Banner></Banner>
@@ -20,6 +21,8 @@ function App() {
         <Skills></Skills>
         <Services></Services>
         <Work></Work>
+        <Contact></Contact>
+        <Bar></Bar>
       </div>
 
     </>
--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -1,50 +1,25 @@
-import React from 'react';
-import ProgressBar from "react-animated-progress-bar";
-// import { BsArrowUpRight } from "react-icons/bs";
+import React, { useState } from 'react';
 // motion
 import { motion } from "framer-motion";
 // variants
 import { fadeIn } from "../variants";
 
 
-
-
-const skills = [
-    {
-        id: 1,
-        title: "React.js",
-        value: 70,
-    },
-    {
-        id: 2,
-        title: "Tailwind",
-        value: 83,
-    },
-    {
-        id: 3,
-        title: "Firebase",
-        value: 76,
-    },
-    {
-        id: 4,
-        title: "Node Js",
-        value: 55,
-    },
-    {
-        id: 5,
-        title: "Express Js",
-        value: 55,
-    },
-    {
-        id: 6,
-        title: "MongoDB",
-        value: 60,
-    },
-];
-
-
-
 const Skills = () => {
+
+    const [skills, setSkills] = useState([
+        { title: 'HTML', percent: 95 },
+        { title: 'CSS', percent: 70 },
+        { title: 'Tailwind CSS', percent: 90 },
+        { title: 'JavaScript', percent: 70 },
+        { title: 'React JS', percent: 80 },
+        { title: 'Node Js', percent: 60 },
+        { title: 'Mongo DB', percent: 70 },
+        { title: 'Next Js', percent: 50 },
+    ]);
+
+    const [currentSkill, setCurrentSkill] = useState({ title: 'HTML', percent: 95 });
+    const circumference = 2 * Math.PI * 120;
 
 
     return (
@@ -60,22 +35,32 @@ const Skills = () => {
                             viewport={{ once: false, amount: 0.3 }}
                         >
                             {/* services list */}
-                            <h2 className="h2 text-accent text-center"> Skills Section </h2>
+                            <h2 className="h2 text-accent text-center"> Skills Section </h2>\
 
-                            <div className="grid grid-cols-2 md:grid-cols-3 gap-4 ">
-                                {skills.map((item) => (
-                                    <div key={item.id} className="text-center shadow-lg p-6  ">
-                                        <div className="flex justify-center">
-                                            <ProgressBar
-                                                width="230"
-                                                trackWidth="13"
-                                                percentage={item.value}
-                                            />
-                                        </div>
-                                        <h3 className="text-2xl font-semibold">{item.title}</h3>
+                            <main className="grid w-full  text-gray-100  place-content-center">
+                                <section className="p-6 space-y-6  rounded-xl md:grid md:grid-cols-2 md:gap-4 sm:space-y-0"
+                                    x-data="skillDisplay">
+                                    <div className="grid grid-cols-2 gap-6">
+                                        {skills.map((skill, index) => (
+                                            <button key={index} onClick={() => setCurrentSkill(skill)}
+                                                className={`px-4 py-2 text-xl text-gray-100 transition bg-blue-600 rounded-md h-14 w-44 hover:bg-blue-700 ${currentSkill.title === skill.title ? 'font-bold ring-2 ring-gray-100' : ''}`}>
+                                                {skill.title}
+                                            </button>
+                                        ))}
                                     </div>
-                                ))}
-                            </div>
+                                    <div className="flex items-center justify-center">
+                                        <svg className="transform -rotate-90 w-72 h-72">
+                                            <circle cx="145" cy="145" r="120" stroke="currentColor" strokeWidth="30" fill="transparent" className="text-gray-700" />
+                                            <circle cx="145" cy="145" r="120" stroke="currentColor" strokeWidth="30" fill="transparent"
+                                                strokeDasharray={circumference} strokeDashoffset={circumference - currentSkill.percent / 100 * circumference}
+                                                className="text-blue-500" />
+                                        </svg>
+                                        <span className="absolute text-5xl">{`${currentSkill.percent}%`}</span>
+                                    </div>
+                                </section>
+                            </main>
+
+
                         </motion.div>
                     </div>
                 </div>
